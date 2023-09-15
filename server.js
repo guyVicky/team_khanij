186bcf21c128b5fd55e66d903090af46e5c566bd
EliteMandy <manusingh9812210329@gmail.com>
2023-09-15 16:31:06 +0530

    improving home ui

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.get("/home", (req, res) => {
   res.render("home", {style: "static/home-style.css", title: "Coal Shipment"});
 });
 
+app.get("/home2", (req, res) => {
+  res.render("home2");
+});
+
 app.listen(port, () => {
   console.log("Server started on port", port);
 });

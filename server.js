b185cf47bc14d47299ec54fc48cb00b263a1f543
guyVicky <vikrantsingh1889@gmail.com>
2023-09-16 11:54:59 +0530

    working copy w/ map

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.render("home", {style: "static/home-style.css", title: "Coal Shipment"});
+  res.render("home", {style: "static/home-style.css", title: "Coal Shipment", apiKey: "AIzaSyAtMa94fLngENSTDSzoZGKSh-xhTALOc_Y"});
+});
+
+app.get('/testmap', (req, res) => {
+  res.render('map', { apiKey: 'AIzaSyAtMa94fLngENSTDSzoZGKSh-xhTALOc_Y' });
 });
 
 app.listen(port, () => {
